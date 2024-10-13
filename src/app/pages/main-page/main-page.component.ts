c1285c7b91b913d177ec3072a2f106184b9ad7ce
EmilianoChavarria <20223tn089@utez.edu.mx>
2024-10-12 18:43:13 -0600

    Landing page

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
+public categories:any = [
+  {image: 'assets/img/products/product1.png', name: "Zapatos"},
+  {image: 'assets/img/products/product2.png', name: "Shorts"},
+  {image: 'assets/img/products/product3.png', name: "Bolsas"},
+  {image: 'assets/img/products/product4.webp', name: "Playeras"},
+  {image: 'assets/img/products/product5.png', name: "Pantalones"},
+  {image: 'assets/img/products/product6.webp', name: "Tenis"},
+]
+
 
 }
